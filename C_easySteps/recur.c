e8c21341f594f069aa9beddfea52e0893558d81a
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-08-06 22:09:35 -0500

    function - recursive

---
+++ C_easySteps/recur.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void count_down_from(int num);
+
+int main()
+{
+    int start;
+    printf("enter a positive integer to count down from:");
+    scanf("%d", &start);
+    count_down_from(start);
+    printf("lift off!\n");
+
+    return 0;
+
+}
+
+void count_down_from(int num)
+{
+    printf("%d\n", num);
+    --num;
+    if (num < 0) return;
+    else
+    {
+        count_down_from(num);
+    }
+    
+}
+
+// gcc -o recur recur.c
+// ./recur
