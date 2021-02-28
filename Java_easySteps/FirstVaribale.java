dadb644be6e2cf1b4373c4fc535bf040903cb26c
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-02-27 20:25:49 -0500

    basics on java
    from Book : Java in easy steps 7th edition

---
+++ Java_easySteps/FirstVaribale.java
@@ -0,0 +1,13 @@
+package myjava;
+
+public class FirstVaribale {
+
+	public static void main(String[] args) {
+		String message = "initital value";
+		System.out.println(message);
+		message = "modified value";
+		System.out.println(message);
+
+	}
+
+}
