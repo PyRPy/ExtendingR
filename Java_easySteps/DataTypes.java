2732d02d64f78c87139eed7aa5ee2223de5857cc
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-02-27 20:35:17 -0500

    basic data types

---
+++ Java_easySteps/DataTypes.java
@@ -0,0 +1,21 @@
+package myjava;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		char letter = 'M';
+		String title = "java in easy steps";
+		int number = 365;
+		float decimal =98.6f;
+		boolean result = true;
+		
+		System.out.println("initial is " + letter);
+		System.out.println("book is " + title);
+		System.out.println("days are " + number);
+		System.out.println("temperature is " + decimal);
+		System.out.println("answer is " + result);
+		
+
+	}
+
+}
