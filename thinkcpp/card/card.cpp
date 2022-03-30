eb0e7a2bad5d084e485c33f4d32e691bfdf51a95
PyRPy <44626163+PyRPy@users.noreply.github.com>
2022-03-30 15:52:54 -0500

    update member functions

--- thinkcpp/card/card.cpp
+++ thinkcpp/card/card.cpp
@@ -10,6 +10,7 @@ struct Card {
   Card (int s, int r);
   void print () const;
   bool equals (const Card& c2) const;
+  bool isGreater (const Card& c2) const;
 };
 
 Card::Card () {
@@ -49,14 +50,38 @@ bool Card::equals (const Card& c2) const {
     return (rank == c2.rank && suit == c2.suit);
 }
 
+bool Card::isGreater (const Card& c2) const {
+  // first check the suits
+  if (suit > c2.suit) return true;
+  if (suit < c2.suit) return false;
+
+  // if the suits are equal, check the ranks
+  if (rank > c2.rank) return true;
+  if (rank < c2.rank) return false;
+
+  // if the ranks are also equal, return false
+  return false;
+}
+
 int main() {
     Card card (1, 11);
     card.print();
 
     Card card1 (1, 11);
     Card card2 (1, 11);
+    Card card3 (2, 10);
     if (card1.equals(card2)) {
         cout << "yup, that's the same card." << endl;
+    }
+    if (card2.isGreater(card3)) {
+      card2.print();
+      cout << "is greater than" << endl;
+      card3.print();
+    }
+    else {
+      card3.print();
+      cout << "is great than" << endl;
+      card2.print();
     }
 
     return 0;
