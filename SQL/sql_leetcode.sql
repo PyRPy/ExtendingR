0cdebd215cc0acc05f8d29015703467e60cf23d3
PyRPy <44626163+PyRPy@users.noreply.github.com>
2022-08-08 21:07:54 -0500

    count, group by

--- SQL/sql_leetcode.sql
+++ SQL/sql_leetcode.sql
@@ -47,3 +47,35 @@ where p.email = b.email and p.id > b.id;
 select w.id
 from Weather as w, Weather as b
 where w.temperature > b.temperature and datediff(w.recordDate, b.recordDate) = 1;
+
+
+584. Find Customer Referee
+# Write your MySQL query statement below
+select name
+from Customer
+where (referee_id <> 2) or (referee_id is null);
+
+586. Customer Placing the Largest Number of Orders
+# Write your MySQL query statement below
+select b.customer_number from
+(select customer_number, count(order_number) as cnt from Orders group by customer_number) as b
+order by b.cnt desc
+limit 1;
+
+SELECT customer_number
+FROMorders
+GROUPBY customer_number
+ORDERBY COUNT(*) DESC LIMIT1;
+
+595. Big Countries
+# Write your MySQL query statement below
+SELECT name, population, area 
+FROM World
+where (area >= 3000000) or (population >= 25000000);
+
+596. Classes More Than 5 Students
+# Write your MySQL query statement below
+select class
+from Courses
+group by class
+having count(student) >= 5;
