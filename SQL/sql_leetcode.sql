9d943539b59b8910b43ea45a297d12ad9296b2d5
PyRPy <44626163+PyRPy@users.noreply.github.com>
2022-08-10 21:03:17 -0500

    group by, having...

--- SQL/sql_leetcode.sql
+++ SQL/sql_leetcode.sql
@@ -98,3 +98,10 @@ order by rating desc;
 # Write your MySQL query statement below
 update Salary
 set sex = case when sex = "m" then "f" when sex = "f" then "m" end;
+
+1050. Actors and Directors Who Cooperated At Least Three Times
+# Write your MySQL query statement below
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(timestamp) >= 3;
