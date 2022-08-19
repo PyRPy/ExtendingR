2a4484554f1ff7ae6faebe79a23a410f53015a10
PyRPy <44626163+PyRPy@users.noreply.github.com>
2022-08-18 22:46:37 -0500

    where...and...

--- SQL/sql_leetcode.sql
+++ SQL/sql_leetcode.sql
@@ -225,3 +225,8 @@ select event_day as day, emp_id, sum(out_time - in_time) as total_time
 from Employees
 group by event_day, emp_id;
 
+1757. Recyclable and Low Fat Products
+-- to test conditions
+select product_id
+from Products
+where low_fats = "Y" and recyclable = "Y";
